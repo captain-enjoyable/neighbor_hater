88242c1eb52fb41e70e10d3efc2b32c9ee35aa54
Matt Eddy <matteddy1@gmail.com>
2014-01-16 17:51:42 -0700

    Geocoding service returns data. Tests green.

--- app/models/geocoding_service.rb
+++ app/models/geocoding_service.rb
@@ -1,6 +1,34 @@
 class GeocodingService
-	def self.address_to_lat_long
+	attr :address
+	attr :lat_long
+	attr :location_data
+
+	def initialize (address)
+		@address = address
 	end
-	def self.normalize_location_name (lat_long_hash)
+
+	def self.run (address)
+		self.new(address).create_location_data
+	end
+
+	def create_location_data
+		@lat_long = parse_lat_long(create_request_object(address)) 
+		@normalized_address = parse_normalized_address(create_request_object(@lat_long['lat'].to_s + "," + @lat_long['lng'].to_s))
+		{
+			'lat_long' => @lat_long,
+			'normalized_address' => @normalized_address
+		}
+	end
+
+	def parse_lat_long(request_object)
+		request_object.first.data['geometry']['location']
+	end
+
+	def parse_normalized_address(request_object)
+		request_object.first.data['formatted_address']
+	end
+
+	def create_request_object(query)
+		Geocoder.search(query)
 	end
 end
--- spec/model/geocoding_service_spec.rb
+++ spec/model/geocoding_service_spec.rb
@@ -4,13 +4,13 @@ describe GeocodingService do
 
 	describe ".address_to_lat_long" do 
 		it "SHould return the lat long of an address" do
-		GeocodingService.address_to_lat_long.should("539 University Ave, Missoula MT") == {"lat"=>46.859767, "lng"=>-113.989401}
+			GeocodingService.run('539 University Ave, Missoula MT')['lat_long'].should == {"lat"=>46.859767, "lng"=>-113.989401}
 		end
 	end
 
 	describe ".normalize_location_name" do
 		it "It should return a formalized address name from a lat long" do	
-			GeocodingService.normalize_location_name({"lat"=>46.859767, "lng"=>-113.989401}).should == "539 University Avenue, Missoula, MT 59801, USA"
+			GeocodingService.run('539 University Ave, Missoula MT')['normalized_address'].should == "539 University Avenue, Missoula, MT 59801, USA"
 		end
 	end
 
